94b550877a00012a660022929a0d9bb41152b5a8
Anna Kysilchuk <anya.kislchuk@gmail.com>
2022-02-02 21:39:29 +0200

    #1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const bodyBg = document.querySelector("body");
+let timerId = null;
+const isDisabled = startBtn.hasAttribute("disabled");
 
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    if (!isDisabled) {
+        startBtn.setAttribute("disabled", "disabled");
+        stopBtn.removeAttribute("disabled");
+    }
+
+    bodyBg.style.backgroundColor = getRandomHexColor()
+
+    timerId = setInterval(() => {
+        bodyBg.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function onStopBtnClick() {
+    if (!isDisabled) {
+        startBtn.removeAttribute("disabled");
+        stopBtn.setAttribute("disabled", "disabled");
+    }
+
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
