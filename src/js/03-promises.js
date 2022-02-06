5d2de9dccdd9213332736316e3ba9da3d00cf792
Anna Kysilchuk <anya.kislchuk@gmail.com>
2022-02-06 18:04:32 +0200

    #3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const formData = {};
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit)
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  return formData;
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let k = Number(formData.delay);
+  for (let i = 1; i <= formData.amount; i += 1) {
+    createPromise(i, k)
+      .then((result) => {
+        Notiflix.Notify.success(result);
+      })
+      .catch((result) => {
+        Notiflix.Notify.failure(result);
+      });
+    k += Number(formData.step);
   }
 }
+
+function createPromise(position, delay) {
+  
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+         reject(`Rejected promise ${position} in ${delay}ms`);
+      }     
+     }, delay)
+    }
+  );
+
+}
+
+  
